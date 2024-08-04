e528afc12d164c7b8d2f949184dbfb9fd0b6886c
jhoaho <jhoaho.sanchez@epn.edu.ec>
2024-08-04 11:12:10 -0500

    agregando la cabezera al home

--- ConsoleAppEcuadorMikuna/ecuadormikuna_v2_client/src/pages/HomePage.jsx
+++ ConsoleAppEcuadorMikuna/ecuadormikuna_v2_client/src/pages/HomePage.jsx
@@ -1,50 +1,82 @@
-import React from 'react';
-import '../styles/Home.css';
+import React from "react";
+import Navbar from "../components/Navbar";
+import "../styles/Home.css";
 
 const HomePage = () => {
   return (
-    <div className="home">
-      <div className="hero-section">
-        <img 
-          src="src/assets/Banner01.jpg" 
-          alt="Cafe" 
-          className="hero-image"
-        />
-        <div className="hero-text">
+    <div>
+      <Navbar />
+      <div className="home">
+        <div className="hero-section">
+          <img
+            src="src/assets/Banner01.jpg"
+            alt="Cafe"
+            className="hero-image"
+          />
+          <div className="hero-text">
+            <div>
+              <h1>Bienvenido a Ecuador Mikuna</h1>
+              <p>Sabor, Tradición y Calidad en Cada Taza</p>
+            </div>
+            <button className="cta-button">Explorar Menú</button>
+          </div>
+        </div>
+        <div className="about-section">
           <div>
-            <h1>Bienvenido a Ecuador Mikuna</h1>
-            <p>Sabor, Tradición y Calidad en Cada Taza</p>
+            <p>
+              Bienvenidos a Ecuador Mikuna, una encantadora cafetería
+              ecuatoriana que celebra la riqueza de nuestra cultura y
+              gastronomía. Ubicada en el corazón de la ciudad, ofrecemos una
+              experiencia única donde los aromas y sabores autóctonos se
+              combinan con un ambiente acogedor y auténtico.
+            </p>
           </div>
-          <button className="cta-button">Explorar Menú</button>
+          <div>
+            <p>
+              En Ecuador Mikuna, cada taza de café es un viaje a las fértiles
+              tierras de los Andes y la Amazonía, donde se cultivan nuestros
+              granos de alta calidad. Además de nuestras exquisitas bebidas,
+              deleítese con una selección de platos tradicionales preparados con
+              ingredientes frescos y locales, honrando nuestras raíces
+              culinarias.
+            </p>
+          </div>
+          <div>
+            <p>
+              Ya sea para un desayuno revitalizante, un almuerzo reconfortante o
+              una merienda relajante, Ecuador Mikuna es el lugar perfecto para
+              disfrutar de momentos inolvidables. ¡Ven y descubre el verdadero
+              sabor de Ecuador con nosotros!
+            </p>
+          </div>
+          <h2>Nuestra Historia</h2>
+          <p>
+            En ECUADOR MIKUNA, valoramos la calidad y la tradición. Cada taza de
+            café es una muestra de nuestra dedicación a ofrecerte lo mejor.
+          </p>
         </div>
-      </div>
-      <div className="about-section">
-        <h2>Nuestra Historia</h2>
-        <p>
-          En ECUADOR MIKUNA, valoramos la calidad y la tradición. Cada taza de café es una muestra de nuestra dedicación a ofrecerte lo mejor.
-        </p>
-      </div>
-      <div className="menu-section">
-        <h2>Nuestro Menú</h2>
-        <div className="menu-items">
-          <div className="menu-item">
-            <img src="path_to_item_image.jpg" alt="Cafe Item" />
-            <p>Café Especial</p>
+        <div className="menu-section">
+          <h2>Nuestro Menú</h2>
+          <div className="menu-items">
+            <div className="menu-item">
+              <img src="path_to_item_image.jpg" alt="Cafe Item" />
+              <p>Café Especial</p>
+            </div>
+            <div className="menu-item">
+              <img src="path_to_item_image.jpg" alt="Cafe Item" />
+              <p>Pastelería Fina</p>
+            </div>
+            {/* Añade más items del menú según sea necesario */}
           </div>
-          <div className="menu-item">
-            <img src="path_to_item_image.jpg" alt="Cafe Item" />
-            <p>Pastelería Fina</p>
-          </div>
-          {/* Añade más items del menú según sea necesario */}
         </div>
-      </div>
-      <div className="contact-section">
-        <h2>Contacto</h2>
-        <p>Dirección: Calle Falsa 123, Quito, Ecuador</p>
-        <p>Teléfono: (02) 123-4567</p>
+        <div className="contact-section">
+          <h2>Contacto</h2>
+          <p>Dirección: Calle Falsa 123, Quito, Ecuador</p>
+          <p>Teléfono: (02) 123-4567</p>
+        </div>
       </div>
     </div>
   );
-} 
+};
 
 export default HomePage;
